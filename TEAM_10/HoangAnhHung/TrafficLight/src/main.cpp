38270df21593c441bbb1010b8c4497159e42c3c2
vvdung <vvdung@husc.edu.vn>
2025-02-21 07:44:13 +0700

    vvdung

--- TEAM_10/HoangAnhHung/TrafficLight/src/main.cpp
+++ TEAM_10/HoangAnhHung/TrafficLight/src/main.cpp
@@ -5,18 +5,63 @@
 #define yPin 18
 #define gPin 17
 
-#define rTime 7
-#define yTime 2
-#define gTime 5
+#define buttonPin 13  // the number of the pushbutton pin
+
+#define  rTime 7
+#define  yTime 2
+#define  gTime 5
 
 #define CLK 25
 #define DIO 26
 
 TM1637Display display(CLK, DIO);
 
+// variables will change:
+
+int buttonState = LOW;  // variable for reading the pushbutton status
+int currentPin = rPin;
+int timeWaiting = rTime;
+ulong ledStart = 0;
+
+bool isReady(ulong& tStart, uint32_t ms){
+  ulong t = millis();
+  if (t - tStart < (ulong)ms) return false;
+  tStart = t;
+  return true;
+}
+
+void TurnOnLed(){
+
+}
+
 /* keep the signal light on during a period of time displayed on seven-segment display
   `(int) s` the signal light's on time (second)
 */
+bool bClearDisplay = false;
+bool isOffDisplay(){
+  if (buttonState == LOW){
+    if (!bClearDisplay){ 
+      display.clear();
+      Serial.println("Display.Clear");
+    }
+    bClearDisplay = true;
+    return true;
+  }   
+  bClearDisplay = false;
+  return false;
+}
+
+ulong timeCountDown = 0;
+void ShowCountdown(){
+  if (timeWaiting < 0) return;
+  if (isOffDisplay()) return;
+  if (!isReady(timeCountDown, 1000)) return;
+  display.showNumberDec(timeWaiting, true, 2, 2);
+  Serial.println(timeWaiting);
+  --timeWaiting;
+  
+}
+
 void countdown(int s) {
   while (s >= 0) {
     display.showNumberDec(s, true, 2, 2);
@@ -36,19 +81,22 @@ void countdown(int s) {
     - `r` turn on the red light
 */
 void turnOn(char signal) {
-  if (signal == 'G') {
+
+  if (signal == 'G') {        
+    currentPin = gPin;
     digitalWrite(rPin, LOW);
     digitalWrite(yPin, LOW);
     digitalWrite(gPin, HIGH);
     Serial.println("Green:");
+    
   }
-  else if (signal == 'Y') {
+  else if (signal == 'Y') {    
     digitalWrite(rPin, LOW);
     digitalWrite(yPin, HIGH);
     digitalWrite(gPin, LOW);
     Serial.println("Yellow:");
   }
-  else if (signal == 'R') {
+  else if (signal == 'R') {    
     digitalWrite(rPin, HIGH);
     digitalWrite(yPin, LOW);
     digitalWrite(gPin, LOW);
@@ -69,6 +117,7 @@ void TrafficLight() {
 
 //Traffic light with seven-segment display to show the remaining time of the signal light
 void TrafficLight_ver2() {
+
   char signal[3] = {'G', 'Y', 'R'};
   for (int i = 0; i < 3; i++) {
     turnOn(signal[i]);
@@ -79,8 +128,40 @@ void TrafficLight_ver2() {
 }
 
 // Traffic light with code that dosen't use `delay()` function
-void TrafficLight_ver3() {
 
+
+//int ledIndex = 2;
+
+void TrafficLight_ver3() {    
+  
+  if (currentPin == rPin && isReady(ledStart, rTime * 1000)) {    
+    digitalWrite(yPin, LOW);
+    digitalWrite(rPin, HIGH);
+    currentPin = gPin;    
+    Serial.println("RED");
+    timeWaiting = rTime - 1;
+  }
+  else if (currentPin == gPin && isReady(ledStart, gTime * 1000)){    
+    digitalWrite(rPin, LOW);
+    digitalWrite(gPin, HIGH);
+    currentPin = yPin;
+    Serial.println("GREE");
+    timeWaiting = gTime - 1;
+  }
+  else if (currentPin == yPin && isReady(ledStart, yTime * 1000)){
+    digitalWrite(gPin, LOW);
+    digitalWrite(yPin, HIGH);
+    currentPin = rPin;
+    Serial.println("YELL");
+    timeWaiting = yTime - 1;    
+  }
+  ShowCountdown();
+
+  //turnOn(signal[ledIndex]);
+  // if (signal[ledIndex] == 'G') countdown(gTime);
+  // else if (signal[ledIndex] == 'Y') countdown(yTime);
+  // else countdown(rTime);
+  //if (++ledIndex > 2) ledIndex = 0;
 }
 
 void setup() {
@@ -88,11 +169,27 @@ void setup() {
   pinMode(rPin, OUTPUT);
   pinMode(yPin, OUTPUT);
   pinMode(gPin, OUTPUT);
+
+  pinMode(buttonPin, INPUT);
+
+  digitalWrite(rPin, LOW);
+  digitalWrite(yPin, LOW);
+  digitalWrite(gPin, LOW);
+
   display.setBrightness(7);
 }
 
+int prevButton = 0;
 void loop() {
-  //TrafficLight();
-  TrafficLight_ver2();
+  //TrafficLight();  
+  //TrafficLight_ver2();
+
+  int w = digitalRead(buttonPin);
+  if (w == HIGH){
+    buttonState = !buttonState;
+  }
+
+  TrafficLight_ver3();
+  //Serial.println(buttonState);
 }
 
