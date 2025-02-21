d983de5170680f224c751f133e8ee5d45ceda9fd
Thiện Bình <21t1020254@husc.edu.vn>
2025-02-21 10:05:20 +0700

    binh update team 5

--- TEAM_05/ESP32_Traffic_Light/src/main.cpp
+++ TEAM_05/ESP32_Traffic_Light/src/main.cpp
@@ -1,18 +1,57 @@
 #include <Arduino.h>
+#include <TM1637Display.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Định nghĩa chân kết nối
+#define BUTTON_PIN 23
+#define LED_BLUE 21
+#define LED_GREEN 25
+#define LED_YELLOW 26
+#define LED_RED 27
+#define LDR_PIN 13
+#define CLK 18
+#define DIO 19
+
+// Khởi tạo đối tượng hiển thị TM1637
+TM1637Display display(CLK, DIO);
+
+// Biến toàn cục
+int buttonState = 0;
+int lastButtonState = HIGH;
+bool displayOn = true;
+int ldrValue = 0;
+int countdown = 10;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+unsigned long blinkMillis = 0;
+const long blinkInterval = 500;
+const long debounceDelay = 50;
+unsigned long lastDebounceTime = 0;
+bool yellowBlinkState = false;
+
+enum TrafficLightState { GREEN, YELLOW, RED };
+TrafficLightState lightState = GREEN;
+
+void setupPins() {
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+    pinMode(LED_YELLOW, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LDR_PIN, INPUT);
+    display.setBrightness(0x0F);
+    display.showNumberDec(countdown);
+    Serial.begin(115200);
+}
+
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    setupPins();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    readLDR();
+    checkButtonPress();
+    updateCountdown();
+    handleTrafficLights();
 }
