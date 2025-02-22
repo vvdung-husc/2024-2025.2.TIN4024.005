cc2242c80a272949e42d139924446bb14791be99
PhamNgocCam <21t1020256@husc.edu.vn>
2025-02-22 16:42:16 +0700

    update_main_trafficlight

--- TEAM_06/ESP32_Traffic_Light/src/main.cpp
+++ TEAM_06/ESP32_Traffic_Light/src/main.cpp
@@ -1,4 +1,20 @@
 #include <Arduino.h>
+#include <TM1637Display.h>
+
+//Định nghĩa các chân kết nối
+#define BTUTTON_PIN 23
+#define LED_RED 27
+#define LED_YELLOW 26
+#define LED_BLUE 25
+#define LED_PURPLE 21
+
+#define LDR_PIN 13
+
+#define CLK 18
+#define DIO 19
+
+//TML1637 4-wire interface
+TM1637Display display(CLK,DIO);
 
 // put function declarations here:
 int myFunction(int, int);
