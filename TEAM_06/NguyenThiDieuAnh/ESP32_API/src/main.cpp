6f2b73f70f7fee19923ec1c550c024611f274894
DieuAnh1609 <21T1020113@husc.edu.vn>
2025-03-11 17:47:44 +0700

    diuanh

--- TEAM_06/NguyenThiDieuAnh/ESP32_API/src/main.cpp
+++ TEAM_06/NguyenThiDieuAnh/ESP32_API/src/main.cpp
@@ -1,20 +1,60 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-int ledPin = 5;
-void setup() {
-  // put your setup code here, to run once:
+const char *ssid = "Wokwi-GUEST";
+const char *password = "";
+const char *apiUrl = "http://ip4.iothings.vn/?geo=1";
+
+void setup()
+{
   Serial.begin(115200);
-  pinMode(ledPin,OUTPUT);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected to WiFi");
+
+  HTTPClient http;
+  http.begin(apiUrl);
+
+  int httpCode = http.GET();
+
+  if (httpCode > 0)
+  {
+    String payload = http.getString();
+    Serial.println("HTTP Response: " + payload);
+
+    // Phân tích chuỗi phản hồi
+    int lonStart = payload.lastIndexOf('|', payload.lastIndexOf('|') - 1) + 1;
+    int latStart = payload.lastIndexOf('|') + 1;
+
+    String longitude = payload.substring(lonStart, latStart - 1);
+    String latitude = payload.substring(latStart);
+
+    longitude.trim();
+    latitude.trim();
+
+    Serial.println("Latitude: " + latitude);
+    Serial.println("Longitude: " + longitude);
+
+    String googleMapsLink = "http://www.google.com/maps/place/" + latitude + "," + longitude;
+    Serial.println("Google Maps Link: " + googleMapsLink);
+  }
+  else
+  {
+    Serial.print("HTTP GET request failed, error: ");
+    Serial.println(http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
- digitalWrite(ledPin, HIGH);
- Serial.println("LED ON");
- delay(1000);
- digitalWrite(ledPin, LOW);
- Serial.println("LED OFF");
- delay(1000);
-
+void loop()
+{
+  // Do nothing in loop
 }
-
