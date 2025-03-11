bdbd783db6edc2074ba7eb771e0dfbcc2ff50c90
phanduyan2511@gmail.com <124804965+PhanDuyAn2511@users.noreply.github.com>
2025-03-11 16:29:47 +0700

    add project_blynk_traffic

---
+++ TEAM_06/Project_Traffic_Blynk/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+}
+
+void loop() {
+
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+}
