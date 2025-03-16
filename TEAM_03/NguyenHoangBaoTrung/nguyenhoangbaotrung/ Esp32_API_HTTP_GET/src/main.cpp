ccc405c1f0a745306ba94f1d1238ae8130c58c1b
Nguyễn Hoàng Bảo Trung <nguyenbaotrung200@gmail.com>
2025-03-16 19:34:34 +0700

    update

--- TEAM_03/NguyenHoangBaoTrung/nguyenhoangbaotrung/ Esp32_API_HTTP_GET/src/main.cpp
+++ TEAM_03/NguyenHoangBaoTrung/nguyenhoangbaotrung/ Esp32_API_HTTP_GET/src/main.cpp
@@ -1,18 +1,60 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char *ssid = "Wokwi-GUEST";
+const char *password = "";
+const char *apiUrl = "http://ip4.iothings.vn/?geo=1";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected to WiFi");
+
+  HTTPClient http;
+  http.begin(apiUrl);
+
+  int httpCode = http.GET();
+
+  if (httpCode > 0)
+  {
+    String payload = http.getString();
+    Serial.println("HTTP Response: " + payload);
+
+    // Phân tích chuỗi phản hồi
+    int lonStart = payload.lastIndexOf('|', payload.lastIndexOf('|') - 1) + 1;
+    int latStart = payload.lastIndexOf('|') + 1;
+
+    String longitude = payload.substring(lonStart, latStart - 1);
+    String latitude = payload.substring(latStart);
+
+    longitude.trim();
+    latitude.trim();
+
+    Serial.println("Latitude: " + latitude);
+    Serial.println("Longitude: " + longitude);
+
+    String googleMapsLink = "http://www.google.com/maps/place/" + latitude + "," + longitude;
+    Serial.println("Google Maps Link: " + googleMapsLink);
+  }
+  else
+  {
+    Serial.print("HTTP GET request failed, error: ");
+    Serial.println(http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // Do nothing in loop
 }
