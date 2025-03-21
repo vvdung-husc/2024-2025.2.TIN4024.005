b1802e3ca0b92510c8c23fbebbb9c8a6f595ac6e
Thiện Bình <21t1020254@husc.edu.vn>
2025-03-21 10:15:18 +0700

    bình commit

--- TEAM_05/ESP8266_Project_Blynk/src/main.cpp
+++ TEAM_05/ESP8266_Project_Blynk/src/main.cpp
@@ -6,14 +6,20 @@
 
 #include <Wire.h>
 #include <U8g2lib.h>
-//Lê Quang Khải
-#define BLYNK_TEMPLATE_ID "TMPL6JA7z9_KD"
-#define BLYNK_TEMPLATE_NAME "ESP8266 Project Blynk"
-#define BLYNK_AUTH_TOKEN "XeOcIK_VvI8815fDjcW4iTYbsysNE30z"
+// //Lê Quang Khải
+// #define BLYNK_TEMPLATE_ID "TMPL6JA7z9_KD"
+// #define BLYNK_TEMPLATE_NAME "ESP8266 Project Blynk"
+// #define BLYNK_AUTH_TOKEN "XeOcIK_VvI8815fDjcW4iTYbsysNE30z"
 //Lê Phước Quang
-#define BLYNK_TEMPLATE_ID "TMPL6wQKPQ6OH"
-#define BLYNK_TEMPLATE_NAME "ESP8286 Project Blynk"
-#define BLYNK_AUTH_TOKEN "AyPfhrFYJN8w_ECXOODxvJpFVSDu5dEe"
+// #define BLYNK_TEMPLATE_ID "TMPL6wQKPQ6OH"
+// #define BLYNK_TEMPLATE_NAME "ESP8286 Project Blynk"
+// #define BLYNK_AUTH_TOKEN "AyPfhrFYJN8w_ECXOODxvJpFVSDu5dEe"
+
+//Lê Nguyễn Thiện Bình
+#define BLYNK_TEMPLATE_ID "TMPL6BB21OMBX"
+#define BLYNK_TEMPLATE_NAME "ESP8266 Project Blynk"
+#define BLYNK_AUTH_TOKEN "kgdfoQHneDMkL5gIAHWlL33IkVKT7pT3"
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp8266.h>
@@ -73,4 +79,61 @@ void setup() {
 
     oled.sendBuffer();
     randomSeed(analogRead(0));
+}
+void chopTatCaDen() {
+    static ulong thoiGianTruoc = 0;
+    static int denHienTai = 0;
+    static const int danhSachDen[3] = {LED_XANH, LED_VANG, LED_DO};
+
+    if (!IsReady(thoiGianTruoc, 1000)) return;
+    int denTruoc = (denHienTai + 2) % 3;
+    digitalWrite(danhSachDen[denTruoc], LOW);
+    digitalWrite(danhSachDen[denHienTai], HIGH);
+    denHienTai = (denHienTai + 1) % 3;
+}
+
+float doAm = 0.0;
+float nhietDo = 0.0;
+
+void capNhatDHT() {
+    static ulong thoiGianTruoc = 0;
+    if (!IsReady(thoiGianTruoc, 2000)) return;
+
+    float doAmMoi = random(0, 10001) / 100.0;
+    float nhietDoMoi = random(-4000, 8001) / 100.0;
+
+    if (isnan(doAmMoi) || isnan(nhietDoMoi)) {
+        Serial.println("Lỗi đọc cảm biến DHT!");
+        return;
+    }
+
+    bool canVe = false;
+
+    if (nhietDo != nhietDoMoi) {
+        canVe = true;
+        nhietDo = nhietDoMoi;
+        Serial.printf("Nhiệt độ: %.2f °C\n", nhietDo);
+    }
+
+    if (doAm != doAmMoi) {
+        canVe = true;
+        doAm = doAmMoi;
+        Serial.printf("Độ ẩm: %.2f%%\n", doAm);
+    }
+
+    if (canVe) {
+        oled.clearBuffer();
+        oled.setFont(u8g2_font_unifont_t_vietnamese2);
+
+        String chuoiNhietDo = StringFormat("Nhiệt độ: %.2f °C", nhietDo);
+        oled.drawUTF8(0, 14, chuoiNhietDo.c_str());
+
+        String chuoiDoAm = StringFormat("Độ ẩm: %.2f%%", doAm);
+        oled.drawUTF8(0, 42, chuoiDoAm.c_str());
+
+        oled.sendBuffer();
+    }
+
+    Blynk.virtualWrite(V1, nhietDo);
+    Blynk.virtualWrite(V2, doAm);
 }
