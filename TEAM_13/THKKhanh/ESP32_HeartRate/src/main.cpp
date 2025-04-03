67e15899d4771d7268d05b415988793634a87153
klenhs03 <kimkhanh151003@gmail.com>
2025-04-03 14:06:35 +0700

    thkkhanh

--- TEAM_13/THKKhanh/ESP32_HeartRate/src/main.cpp
+++ TEAM_13/THKKhanh/ESP32_HeartRate/src/main.cpp
@@ -1,11 +1,18 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
 void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    Serial.println("Hello, ESP32!");
-  }
-  
-  void loop() {
-    // put your main code here, to run repeatedly:
-    delay(10); // this speeds up the simulation
-  }
-  
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
